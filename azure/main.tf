9a6ff7b7733056ced739cc07062afdc8a782a91f
Dmitry <dmytro.vaga@gmail.com>
2025-01-29 12:09:58 -0700

    nsg - network security group

--- azure/main.tf
+++ azure/main.tf
@@ -40,6 +40,25 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
+# Create a Network Security Group (NSG) to allow SSH traffic
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22" # Allow SSH traffic on port 22
+    source_address_prefix      = "*"  # Allow traffic from any source (restrict this in production!)
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_virtual_machine" "example" {
   name                  = "example-vm"
   location              = azurerm_resource_group.example.location
